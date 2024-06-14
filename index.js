f413057be4fd0a5cec3fc071ae5b59ab2c4777a6
ZahraJou <zahrajoulaei@gmail.com>
2024-06-14 15:29:05 -0400

    part3

--- index.js
+++ index.js
@@ -1,26 +1,89 @@
-const adventurer = {
-  name: "Robin",
-  health: 10,
-  inventory: ["sword", "potion", "artifact"],
-  companion: {
-    name: "Leo",
-    type: "Cat",
-    companion: {
-      name: "Frank",
-      type: "Flea",
-      inventory: ["hat", "sunglasses"],
-    },
-  },
-  roll(mod = 0) {
-    const result = Math.floor(Math.random() * 20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`);
-  },
-};
+// const Adventurer = {
+//   name: "Robin",
+//   health: 10,
+//   inventory: ["sword", "potion", "artifact"],
+//   companion: {
+//     name: "Leo",
+//     type: "Cat",
+//     companion: {
+//       name: "Frank",
+//       type: "Flea",
+//       inventory: ["hat", "sunglasses"],
+//     },
+//   },
+//   roll(mod = 0) {
+//     const result = Math.floor(Math.random() * 20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`);
+//   },
+// };
 
-adventurer.roll()
+// Adventurer.roll()
 
 // console.log(adventurer.companion.companion.inventory[1]);
 
 // adventurer.inventory.forEach((item) => {
 //   console.log(item);
 // });
+
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+}
+
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// robin.roll(1)
+// const frank = robin.companion.companion
+// frank.roll(1)
+// const leo = robin.companion
+// leo.roll(1);
+
+class Adventurer extends Character {
+  constructor(name, role) {
+    super(name);
+    this.role = role;
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
+
+const robin = new Adventurer("Robin");
+
+
+console.log(robin.name, robin.inventory);
+console.log(robin.health);
+
+class Companion extends Character {
+  constructor(name, role) {
+    super(name);
+    this.role = role;
+
+   
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
+const leo = new Companion('Leo')
+console.log(leo.name)
+
+
+
+
